ddb299c4c2bb57dedf02ac53a3499a13fbb42ddd
Rintarooo <310rnomeado@gmail.com>
2023-08-21 16:30:34 +0900

    feat: add distance from cam to screen

--- sample4.py
+++ sample4.py
@@ -5,43 +5,25 @@ def degree2radian(deg):
     # return deg * 3.141592659 /180.
     return deg * np.pi / 180.
 
+def normalize(x):
+    return x / np.linalg.norm(x)
+
 def plotly_plot():
-
-    # uvz = np.array([1,1,1])
-    
-    uvz = [[1],[1],[1]]
-    xyz = [[0],[0],[0]]
-    # fig = go.Figure(data = go.Cone(
-    #     x=xyz[0],  # 矢印の始点のx座標
-    #     y=xyz[1],  # 矢印の始点のy座標
-    #     z=xyz[2],  # 矢印の始点のz座標
-    #     u=uvz[0],  # 矢印の終点のx座標
-    #     v=uvz[1],  # 矢印の終点のy座標
-    #     w=uvz[2],  # 矢印の終点のz座標
-    #     sizemode="absolute",
-    #     sizeref=0.2,
-    #     anchor="tail"))
-
-    # fig = go.Figure(data = go.Cone(
-    #     x=[1, 2],  # 矢印の始点のx座標
-    #     y=[1, 2],  # 矢印の始点のy座標
-    #     z=[1, 2],  # 矢印の始点のz座標
-    #     u=[1, 0],  # 矢印の終点のx座標
-    #     v=[1, 1],  # 矢印の終点のy座標
-    #     w=[1, 1],  # 矢印の終点のz座標
-    #     sizemode="absolute",
-    #     sizeref=0.2,
-    #     anchor="tail"))
-
     camera_pos = np.array([0, 0, 0])
     camera_lookat = np.array([0, 0, -1])
     camera_up = np.array([0, 1, 0])
-    look_dir = camera_lookat - camera_pos
+    look_dir = normalize(camera_lookat - camera_pos)
     camera_right = np.cross(look_dir, camera_up)
 
-    # camera_pos_x = [camera_pos[0] for _ in range(3)]
-    # camera_pos_y = [camera_pos[1] for _ in range(3)]
-    # camera_pos_z = [camera_pos[2] for _ in range(3)]
+    fov = 45
+
+    # カメラからスクリーンまでの距離
+    dist_camera2plane = 1. / (2. * np.tan(degree2radian(fov) * 0.5))
+    print("degree: ", fov)
+    print("radian: ", degree2radian(fov))
+    print(dist_camera2plane)
+    print(look_dir*dist_camera2plane)
+
     
 
     # fig = go.Figure(data = go.Cone(
@@ -186,20 +168,7 @@ def plotly_plot():
 
 if __name__=="__main__":
 
-    # camera_pos = (1, 1, 1)
-    # camera_lookat = (0, 0, 0)
-    # camera_up = (0, 1, 0)
-    camera_pos = np.array([0, 0, 1])
-    camera_lookat = np.array([0, 0, -1])
-    camera_up = np.array([0, 1, 0])
-    look_dir = camera_lookat - camera_pos
-    camera_right = np.cross(look_dir, camera_up)
 
-    fov = 45
-
-    # カメラからスクリーンまでの距離
-    dist_camera2plane = 1. / (2. * np.tan(degree2radian(fov) * 0.5))
-    print(dist_camera2plane)
 
     # https://jun-networks.hatenablog.com/entry/2021/04/02/043216
     plotly_plot()
